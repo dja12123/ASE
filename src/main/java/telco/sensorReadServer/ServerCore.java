224a9641a0c1826a6b31358089bd0a14711a8d1c
dja12123 <dja12123@172.16.1.1>
2019-01-24 22:15:57 +0900

    porttest

--- src/main/java/telco/sensorReadServer/ServerCore.java
+++ src/main/java/telco/sensorReadServer/ServerCore.java
@@ -1,13 +1,17 @@
 package telco.sensorReadServer;
 
+import java.util.Enumeration;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.comm.CommPortIdentifier;
+
 import telco.sensorReadServer.appConnect.AppConnectManager;
 import telco.sensorReadServer.console.LogWriter;
 import telco.sensorReadServer.sensorReader.SensorReadManager;
+import telco.sensorReadServer.sensorReader.SerialReader;
 
 public class ServerCore
 {
@@ -15,8 +19,43 @@ public class ServerCore
 	public static final ExecutorService mainThreadPool = Executors.newCachedThreadPool();
 
 	private static ServerCore mainInst;
-
+	
+	
+	static CommPortIdentifier portId;
+	static Enumeration portList;
 	public static void main(String[] args)
+	{
+		boolean portFound = false;
+		String defaultPort = "/dev/ttyACM0";
+
+		if (args.length > 0)
+		{
+			defaultPort = args[0];
+		}
+
+		portList = CommPortIdentifier.getPortIdentifiers();
+
+		while (portList.hasMoreElements())
+		{
+			portId = (CommPortIdentifier) portList.nextElement();
+			if (portId.getPortType() == CommPortIdentifier.PORT_SERIAL)
+			{
+				if (portId.getName().equals(defaultPort))
+				{
+					System.out.println("Found port: " + defaultPort);
+					portFound = true;
+					SerialReader reader = new SerialReader();
+				}
+			}
+		}
+		if (!portFound)
+		{
+			System.out.println("port " + defaultPort + " not found.");
+		}
+
+	}
+	
+	/*public static void main(String[] args)
 	{
 
 		mainInst = new ServerCore();
@@ -35,7 +74,7 @@ public class ServerCore
 		{
 			e.printStackTrace();
 		}
-	}
+	}*/
 
 	public static void endProgram()
 	{
