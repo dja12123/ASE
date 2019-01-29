1020d85e2a79b1eda9644d4fedb071e5925c4021
dja12123 <dja12123@dja-note>
2019-01-29 15:18:53 +0900

    test

--- src/main/java/telco/sensorReadServer/ServerCore.java
+++ src/main/java/telco/sensorReadServer/ServerCore.java
@@ -169,7 +169,7 @@ public class ServerCore
 
 	private void shutdown()
 	{
-		synchronized (logger)
+		synchronized (System.out)
 		{
 			logger.log(Level.INFO, "시스템 종료 시작");
 			this.appConnectManager.stopModule();
