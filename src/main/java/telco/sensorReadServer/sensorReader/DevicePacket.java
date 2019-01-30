6c1a12ab1332ec36b4d0d57c03ea37e4a56098aa
dja12123 <dja12123@dja-note>
2019-01-30 16:17:58 +0900

    .

---
+++ src/main/java/telco/sensorReadServer/sensorReader/DevicePacket.java
@@ -0,0 +1,63 @@
+package telco.sensorReadServer.sensorReader;
+
+import java.nio.ByteBuffer;
+
+public class DevicePacket
+{
+	public static final int FULL_PACKET_SIZE = 32;
+	
+	public final int ID;
+	public final int NSIZE;
+	public final float X_GRADIANT;
+	public final float Y_GRADIANT;
+	public final float X_ACCEL;
+	public final float Y_ACCEL;
+	public final float Z_ACCEL;
+	public final float Altitiude;
+	
+	public static boolean isDevicePacket(byte[] packet)
+	{
+		if(packet.length != FULL_PACKET_SIZE)
+		{
+			return false;
+		}
+		ByteBuffer buffer = ByteBuffer.wrap(packet);
+		buffer.position(4);
+		if(buffer.getInt() != FULL_PACKET_SIZE)
+		{
+			return false;
+		}
+		return true;
+	}
+	
+	public DevicePacket(byte[] packet)
+	{
+		ByteBuffer buffer = ByteBuffer.wrap(packet);
+		
+		this.ID = buffer.getInt();
+		this.NSIZE = buffer.getInt();
+		this.X_GRADIANT = buffer.getFloat();
+		this.Y_GRADIANT = buffer.getFloat();
+		this.X_ACCEL = buffer.getFloat();
+		this.Y_ACCEL = buffer.getFloat();
+		this.Z_ACCEL = buffer.getFloat();
+		this.Altitiude = buffer.getFloat();
+	}
+	
+	@Override
+	public String toString()
+	{
+		StringBuffer buffer = new StringBuffer();
+		
+		buffer.append("ID: "); buffer.append(this.ID); buffer.append('\n');
+		buffer.append("NSIZE: "); buffer.append(this.NSIZE); buffer.append('\n');
+		buffer.append("X_GRADIANT: "); buffer.append(this.X_GRADIANT); buffer.append('\n');
+		buffer.append("Y_GRADIANT: "); buffer.append(this.Y_GRADIANT); buffer.append('\n');
+		buffer.append("X_ACCEL: "); buffer.append(this.X_ACCEL); buffer.append('\n');
+		buffer.append("Y_ACCEL: "); buffer.append(this.Y_ACCEL); buffer.append('\n');
+		buffer.append("Z_ACCEL: "); buffer.append(this.Z_ACCEL); buffer.append('\n');
+		buffer.append("Altitiude: "); buffer.append(this.Altitiude); buffer.append('\n');
+		
+		return buffer.toString();
+	}
+}
