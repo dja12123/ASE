ad5461d3dd8dec6a241fd166d76aa423d4ac56d2
dja12123 <dja12123@dja-univ-main>
2019-04-02 22:38:47 +0900

    test

--- resources/www/CommModule.mjs
+++ resources/www/CommModule.mjs
@@ -11,7 +11,7 @@ export class CommModule
 		var storage = window.sessionStorage;
 		var storageSession = storage.getItem(INNO_STORAGE_SESSION);
 		
-		if(storageSession === null)
+		if(storageSession === null || typeof(storageSession) != Session)
 		{
 			console.log("loaded6");
 			this.httpGet(CONTROL_GET_UUID_REQUEST, (uid) =>
