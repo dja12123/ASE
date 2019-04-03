c86643f5a858b059eade7c2699be8516cef2b0f2
dja12123 <dja12123@dja-univ-main>
2019-04-03 18:35:20 +0900

    test

--- src/main/java/ase/sensorReadServer/appService/serviceInstance/SensorListSender.java
+++ src/main/java/ase/sensorReadServer/appService/serviceInstance/SensorListSender.java
@@ -1,47 +1,53 @@
 package ase.sensorReadServer.appService.serviceInstance;
 
-import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Consumer;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
 import ase.clientSession.IChannel;
-import ase.sensorReadServer.appService.AppServiceDefine;
 import ase.sensorReadServer.sensorManager.SensorManager;
 import ase.sensorReadServer.sensorManager.sensor.Sensor;
 
 public class SensorListSender extends ServiceInstance
 {
 	public static final String KEY = "SensorListRequest";
-	
+
 	private final SensorManager sensorManager;
-	
+
 	public SensorListSender(Consumer<ServiceInstance> destoryCallback, IChannel channel, SensorManager sensorManager)
 	{
 		super(KEY, destoryCallback, channel);
 		this.sensorManager = sensorManager;
 	}
-	
+
 	@Override
 	protected void onDestroy()
 	{
-		
+
 	}
 
 	@Override
 	protected void onStartService()
 	{
+		JsonObject json = new JsonObject();
+		JsonArray dataSensorList = new JsonArray();
+
 		List<Sensor> sensorList = new ArrayList<>(this.sensorManager.sensorMap.size());
 		sensorList.addAll(this.sensorManager.sensorMap.values());
-		ByteBuffer buf = ByteBuffer.allocate(1+4+(sensorList.size()*(4 + 1)));
-		buf.put(AppServiceDefine.SensorDeviceData_REP_LIST);
-		buf.putInt(sensorList.size());
-		for(Sensor sensor : sensorList)
+		json.addProperty("count", sensorList.size());
+		json.add("data", dataSensorList);
+		for (Sensor sensor : sensorList)
 		{
-			buf.putInt(sensor.id);
-			buf.put((byte)(sensor.isOnline() ? 1 : 0));
+			JsonObject data = new JsonObject();
+			data.addProperty("id", sensor.id);
+			data.addProperty("on", sensor.isOnline());
+			dataSensorList.add(data);
 		}
-		this.channel.sendData(buf.array());
+		
+		this.channel.sendData(json.toString().getBytes());
 		this.destroy();
 	}
 }
