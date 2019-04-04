a368e0ac31478e717b2779f3f5689c5305f0c4ed
hyun <hyun@DESKTOP-NV99QTC>
2019-04-04 17:37:39 +0900

    web update

---
+++ resources/www/js/developer_sensor.js
@@ -0,0 +1,24 @@
+var pos = [], posMax = 7;
+
+window.onload = function() {
+    setState();
+
+    for (let i = 0; i < posMax; i++) {
+        pos[i] = Math.random()*100;
+    }
+    setElem(pos);
+    setDate("190222133235");
+    for(let i=0; i<150; i++)
+        addLog("190222133235", "센서 온라인");
+}
+
+setInterval(function() {
+    setElem(pos);
+}, 300)
+
+setInterval(function() {
+    for (let i = 0; i < posMax; i++) {
+        pos[i] += Math.random()/10;
+    }
+}, 150)
+
--- resources/www/js/item.js
+++ resources/www/js/item.js
@@ -1,57 +1,62 @@
-/*
-서버 -> 클라이언트 -> 웹
-클라이언트 단에서 데이터 형식을 보내면 웹 형식 작성
-
-실시간 수치 변화 -> 데이터를 지속적으로 받음 -> roadData 
-데이터를 지속적으로 표시 -> js, DOM
-*/
-
-var eItem;
-
-var structItem = {};
-
-var item;
-
-function creatArr(key, num) {
-    item = new Object();
-    creatItem(structItem[key].num);
+//key가 4자리 이하일때 앞에 0을 추가
+function keyForm(num) {
+    num = num + '';
+    return num.length >= 4 ? num : new Array(5 - num.length).join('0') + num;
 }
 
-//센서 속성값
-/*
-function roadData(key, sX, sY, aX, aY, aZ, aH) {
-    structItem[key].num = num;
-    structItem[key].slopX = sX;
-    structItem[key].slopY = sY;
-    structItem[key].accX = aX;
-    structItem[key].accY = aY;
-    structItem[key].accZ = aZ;
-    structItem[key].altiH = aH;
-}
-*/
+var total = 0;
 
-/*
-key값으로 graph에 띄울 데이터 구분 -> 어떤 key값의 버튼으로 눌러졌는지 확인 ->
-버튼 onclink을 함수호출로 수정 -> 페이지전환 함수 작성
-*/
-function addItem(num) {
-    eItem = document.createElement("items");
+//add, del함수 실행시 total체크
+function setTotal() {
+    var msg;
+    if(total>0) msg = total + "개의 센서 확인";
+    else msg = "확인된 센서가 없습니다"
+    document.getElementById("total").innerHTML = msg;
+}
+
+// "센서" 추가
+// * addItem(int, boolean) 센서 키, 작동상태(on/off)
+function addItem(key, on) { //table -> div 수정
+    total++;
+    setTotal();
+    var state = on? "checked": "";
+    var eItem = document.createElement("table");
+    eItem.id = key;
+    eItem.className = 'item';
     eItem.innerHTML = [
-        '<table class="item">',
         '<tbody><tr><td class="title">',
-        num,
+        keyForm(key),
         '</td>',
         '<td></td>',
         '<td>',
         '<label class="switch">',
-        '<input type="checkbox">',
+        '<input id="',
+        key+"stat",
+        '" type="checkbox"',
+        state,
+        '>',
         '<div class="slider round"></div>',
         '</label>',
         '</td>',
         '<td>',
         '<button class="item-btn" onclick="location.href=`sensor.html`">VIEW</button>',
         '</td>',
-        ' </tr></tbody></table>',
+        '</tr></tbody>',
     ].join("");
-    document.getElementById('items').appendChild(eItem);
+    document.getElementById('items').append(eItem);
+}
+
+//  "센서" 지우기
+// ＊delItem(int) 센서 키
+function delItem(key) {
+    total--;
+    setTotal();
+    document.getElementById(key).remove();
+}
+
+//  "작동상태" 변경(on/off)
+// ＊state(int, boolean) 센서 키, 작동상태(on/off)
+function state(key, on) {
+    var sensor = document.getElementById(key+"stat");
+    if(sensor.checked != on) sensor.checked = on;
 }
