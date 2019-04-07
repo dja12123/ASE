546a578361b897e952741823f314410582845a80
hyun <hyun@DESKTOP-NV99QTC>
2019-04-07 23:59:41 +0900

    list.js: 코드 정리, total 수정

--- resources/www/sensor_list/manage_sensor_list.js
+++ resources/www/sensor_list/manage_sensor_list.js
@@ -1,8 +1,7 @@
-var total = 0;
+
 
 //add, del함수 실행시 total체크
-function setTotal()
-{
+function setTotal(total) {
 	var msg;
 	if(total > 0) msg = total + "개의 센서 확인";
 	else msg = "확인된 센서가 없습니다"
@@ -12,8 +11,8 @@ function setTotal()
 
 // "센서" 추가
 // * addItem(String, Boolean) 센서 키, 작동상태(on/off)
-function addItem(key, on) //table -> div 수정
-{
+// table -> div 수정
+function addItem(key, on) {
 	var state = on ? "checked" : "";
 	var eItem = document.createElement("table");
 	eItem.id = key;
@@ -40,24 +39,20 @@ function addItem(key, on) //table -> div 수정
 		'</td>',
 		'</tr></tbody>',
 	].join("");
-	document.getElementById('items')
-		.append(eItem);
+	document.getElementById('items').append(eItem);
 }
 
 // "센서" 지우기
 // ＊delItem(String) 센서 키
-function delItem(key)
-{
+function delItem(key) {
 	total--;
 	setTotal();
-	document.getElementById(key)
-		.remove();
+	document.getElementById(key).remove();
 }
 
 //  "작동상태" 변경(on/off)
 // ＊state(String, Boolean) 센서 키, 작동상태(on/off)
-function state(key, on)
-{
+function state(key, on) {
 	var sensor = document.getElementById("stat"+key);
 	if(sensor.checked != on) sensor.checked = on;
 }
@@ -70,6 +65,7 @@ function listDisconnect() {
     '</div>';
 }
 
+// 재접속
 function listReconnect() {
     document.getElementById("main").style.opacity = 1;
     document.body.removeChild(document.getElementById("disconnected"));
