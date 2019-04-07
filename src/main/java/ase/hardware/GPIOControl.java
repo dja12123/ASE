bed93a42fb281515994a4753b55d7900ea6a592c
dja12123 <dja12123@dja-univ-main>
2019-04-07 21:48:48 +0900

    test

--- src/main/java/ase/hardware/GPIOControl.java
+++ src/main/java/ase/hardware/GPIOControl.java
@@ -36,9 +36,10 @@ public class GPIOControl
 		logger.log(Level.INFO, "gpio 제어기 활성화");
 		this.gpio = GpioFactory.getInstance();
 		this.gpioListener = this::gpioListener;
-		GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_18, // PIN NUMBER
+		GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_24, // PIN NUMBER
 				"MyButton", // PIN FRIENDLY NAME (optional)
 				PinPullResistance.PULL_UP);
+		
 		myButton.addListener(this.gpioListener);
 	}
 	
