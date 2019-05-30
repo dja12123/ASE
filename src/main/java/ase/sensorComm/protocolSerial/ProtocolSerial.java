4893979ee3c26631214169f1d3f3fe2289399706
dja12123 <dja12123@dja-home-main>
2019-05-31 00:05:02 +0900

    test

--- src/main/java/ase/sensorComm/protocolSerial/ProtocolSerial.java
+++ src/main/java/ase/sensorComm/protocolSerial/ProtocolSerial.java
@@ -159,6 +159,11 @@ public class ProtocolSerial extends KeyObservable<Short, ReceiveEvent> implement
 			}
 			catch (InterruptedException e1)
 			{
+				try
+				{
+					Thread.sleep(1000);
+				}
+				catch (InterruptedException e2) {}
 				synchronized (this)
 				{
 					if(this.nowTransaction == null)
--- src/main/java/ase/sensorComm/protocolSerial/SerialWriter.java
+++ src/main/java/ase/sensorComm/protocolSerial/SerialWriter.java
@@ -54,11 +54,6 @@ public class SerialWriter
 		{
 			while(!this.writeQueue.isEmpty())
 			{
-				try
-				{
-					Thread.sleep(SerialProtoDef.SERIAL_DELAY);
-				}
-				catch (InterruptedException e){ }
 				byte[] packet;
 				synchronized (this)
 				{
@@ -76,6 +71,11 @@ public class SerialWriter
 				{
 					e.printStackTrace();
 				}
+				try
+				{
+					Thread.sleep(SerialProtoDef.SERIAL_DELAY);
+				}
+				catch (InterruptedException e){ }
 			}
 			try
 			{
