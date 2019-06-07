b64423e6b21888edaeb8eaf5a8466910733524cb
dja12123 <dja12123@dja-note>
2019-06-07 17:36:18 +0900

    web-bug_fix#1

--- resources/wwwO2/O2SensorList/manageO2_sensorList.js
+++ resources/wwwO2/O2SensorList/manageO2_sensorList.js
@@ -20,7 +20,7 @@ function updateValue(key, data)	{ // 센서 아이디에 따른 값 셋팅 함�
 	var getData=data;
 	var ChemicalStatus=document.getElementById(uniqueID);
 	
-	ChemicalStatus.innerHTML='';
+	//ChemicalStatus.innerHTML='';
 	stats= (data*100) + '%';
 	ChemicalStatus.insertAdjacentHTML('beforeend',stats);
 	checkSafety(data);
@@ -31,7 +31,7 @@ function updateValue(key, data)	{ // 센서 아이디에 따른 값 셋팅 함�
 function checkSafety(value) {
 	
 				var SensorStatus=document.getElementById('SensorStat');
-				SensorStatus.innerHTML='';
+				//SensorStatus.innerHTML='';
 				
 				if(value>=21)
 					content= '<span class="badge badge-primary">Safe/안전</span> </h5>';
