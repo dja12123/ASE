3f0788581dc62f80ea77cff5e5c73646d1722134
dja12123 <dja12123@dja-note>
2019-06-08 02:44:31 +0900

    test#1

--- resources/wwwO2/PerSensorRealTimeGraph/O2_status.js
+++ resources/wwwO2/PerSensorRealTimeGraph/O2_status.js
@@ -36,7 +36,7 @@ function addGraphValue(key, on) {
 
 
 
-function updateValue(key, data)	{ // 센서 아이디에 따른 값 셋팅 함수
+function updateValue(key, xTime, data)	{ // 센서 아이디에 따른 값 셋팅 함수
 	var uniqueID=key;
 	var getData=data*100;
 	
