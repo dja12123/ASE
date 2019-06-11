041e91e894dba124cc9eaa21169b20fee96e160f
dja12123 <dja12123@dja-univ-main>
2019-06-11 19:14:12 +0900

    괄호문제해결

--- resources/wwwO2/Notification/main.js
+++ resources/wwwO2/Notification/main.js
@@ -1,23 +1,26 @@
-window.onload = function(){
+window.onload = function()
+{
     var hw = document.getElementById('hw');
-    hw.addEventListener('click', function(){
+    hw.addEventListener('click', function()
+	{
         alert('Hello world');
     })
 
 
 	// 서비스 워커 등록
-	if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function() {
-    navigator.serviceWorker.register('/sw.js').then(function(registration) {
-      // Registration was successful
-      console.log('ServiceWorker registration successful with scope: ', registration.scope);
-    }, function(err) {
-      // registration failed :(
-      console.log('ServiceWorker registration failed: ', err);
-    });
-  });
-}
-}
-	
-	
+	if ('serviceWorker' in navigator)
+	{
+		window.addEventListener('load', function()
+		{
+			navigator.serviceWorker.register('/sw.js').then(function(registration)
+			{
+				// Registration was successful
+				console.log('ServiceWorker registration successful with scope: ', registration.scope);
+			}, function(err)
+			{
+				// registration failed :(
+				console.log('ServiceWorker registration failed: ', err);
+			});
+		});
+	}
 }
