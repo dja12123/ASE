2ce8b011dea60e279e6cfdf3d0df8e9abf408737
dja12123 <dja12123@dja-univ-main>
2019-06-11 17:08:55 +0900

    xTime

--- resources/wwwO2/PerSensorRealTimeGraph/comm_eachSensorGraph.js
+++ resources/wwwO2/PerSensorRealTimeGraph/comm_eachSensorGraph.js
@@ -61,7 +61,8 @@ window.onload = function()
         {		
 				var splitTime = data.time.split("/");
 				var xTime = new Date(splitTime[0], splitTime[1],splitTime[2],splitTime[3],splitTime[4],splitTime[5],splitTime[6]);
-				console.log("xTime", xTime);
+				console.log(xTime);
+				//updateValue(sensorID, xTime, data.value);
         }
         else
         {
