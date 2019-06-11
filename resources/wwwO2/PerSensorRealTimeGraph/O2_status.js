6ef95334fab79104740f6df85b61634a5a5e7213
dja12123 <dja12123@dja-note>
2019-06-11 18:14:36 +0900

    test

--- resources/wwwO2/PerSensorRealTimeGraph/O2_status.js
+++ resources/wwwO2/PerSensorRealTimeGraph/O2_status.js
@@ -42,8 +42,7 @@ function initCanvas()	{
 function updateValue(key, xTime, data)	{ // 센서 아이디에 따른 값 셋팅 함수
 	var uniqueID=key;
 	var getData=data*100;
-	var StringDate= moment(xTime).format('YYYY-MM-DD HH:mm:ss');
-	// StringDate.toISOString().substring(0,10);
+	var StringDate= xTime.toISOString().substring(0,10);
 	
 	
 	getData=Number.parseFloat(getData).toFixed(2);
