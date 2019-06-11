e26e61e1a5c03d5e315bece6019cb829981d8b8c
dja12123 <dja12123@dja-univ-main>
2019-06-11 19:11:25 +0900

    서비스 워커 등록 테스트

--- resources/wwwO2/Notification/main.js
+++ resources/wwwO2/Notification/main.js
@@ -6,21 +6,17 @@ window.onload = function(){
 
 
 	// 서비스 워커 등록
-	if ('serviceWorker' in navigator && 'PushManager' in window) {
-  console.log('Service Worker and Push is supported');
-
-  navigator.serviceWorker.register('sw.js')
-  .then(function(swReg) {
-    console.log('Service Worker is registered', swReg);
-
-    swRegistration = swReg;
-  })
-  .catch(function(error) {
-    console.error('Service Worker Error', error);
+	if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+    });
   });
-} else {
-  console.warn('Push messaging is not supported');
-  pushButton.textContent = 'Push Not Supported';
+}
 }
 	
 	
