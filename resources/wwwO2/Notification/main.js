ef66d514f3dc22aaf67b8fd341bd93bb2c96cbd9
dja12123 <dja12123@dja-univ-main>
2019-06-11 18:43:13 +0900

    인코딩 문제 해결 서비스워커 등록 테스트

--- resources/wwwO2/Notification/main.js
+++ resources/wwwO2/Notification/main.js
@@ -3,4 +3,25 @@ window.onload = function(){
     hw.addEventListener('click', function(){
         alert('Hello world');
     })
+
+
+	// 서비스 워커 등록
+	if ('serviceWorker' in navigator && 'PushManager' in window) {
+  console.log('Service Worker and Push is supported');
+
+  navigator.serviceWorker.register('sw.js')
+  .then(function(swReg) {
+    console.log('Service Worker is registered', swReg);
+
+    swRegistration = swReg;
+  })
+  .catch(function(error) {
+    console.error('Service Worker Error', error);
+  });
+} else {
+  console.warn('Push messaging is not supported');
+  pushButton.textContent = 'Push Not Supported';
+}
+	
+	
 }
