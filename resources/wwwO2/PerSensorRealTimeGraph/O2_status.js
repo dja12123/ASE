a14037e73d6dff18a8bc30381d853116d4874501
dja12123 <dja12123@dja-note>
2019-06-19 01:19:39 +0900

    test

--- resources/wwwO2/PerSensorRealTimeGraph/O2_status.js
+++ resources/wwwO2/PerSensorRealTimeGraph/O2_status.js
@@ -125,7 +125,7 @@ function updateValue(key, xYear,xMonth,xDay,xHour,xMin,xSec,xMSec, data)	{ // ì„
 	var uniqueID=key;
 	var getData=data*100;
 	var xTime= xYear+"-"+xMonth+"-"+xDay+ " " + xHour +":"+ xMin +":"+ xSec +"."+ xMSec;
-	var SIDText='<h4> Sensor ID : ',uniqueID,'</h4>';
+	var SIDText='<h4> Sensor ID : '+uniqueID+'</h4>';
 	getData=Number.parseFloat(getData).toFixed(2);
 	// Decimal set to 2nd place
 	
